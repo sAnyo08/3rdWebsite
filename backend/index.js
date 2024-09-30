00c3718314d686b86c549c21bdd2473299f788b2
sAnyo08 <sanyofonseca2004@gmail.com>
2024-09-30 20:22:49 +0530

    added database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const mongodb = require("./db");
+
+const app = express();
+mongodb();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.use(express.json());
+app.use("/api", require("./Routes/CreatUser"));
+app.listen(5000, () => {
+  console.log(`Example app listening on port 5000`);
+});
